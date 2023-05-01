3faef2c5d1a0b90d785f010c4ef920602cd9abf6
bluNite <ncrowe175@gmail.com>
2023-05-01 14:21:01 -0400

    bluNite corrections

---
+++ moments_js/assets/js/app.js
@@ -0,0 +1,215 @@
+$(document).ready(() => {
+
+	// variables moment formats date and time
+	var headingTimeDisplay = moment().format("LL:LT");
+	var currentTime = moment().hour();
+
+
+
+	// todays's date using moment format for date
+	$("#todaysDateIs").text("Today's Date Is:   " + headingTimeDisplay);
+
+	// set time in box columns 60  min intervals starting at 900am
+	let timeBoxSequence = {
+		intervalBoxTime: 60,
+		firstEntry: '0800',
+		lastEntry: '1600'
+	};
+
+	//variables for start and finish time, var timeBoxHors for moment start time text
+
+	// time start 900am
+	let startNow = moment(timeBoxSequence.firstEntry, "HH:mm");
+
+
+	// time finishes at 500pm
+	let finishTime = moment(timeBoxSequence.lastEntry, "HH:mm").add(1, 'hour');
+
+
+	// time for boxes 9am-5pm
+	let timeBoxHours = [];
+
+	// timeBoxHours time added
+	while (startNow < finishTime) {
+		//moment start and finish time formatted , time added to time box 
+		startNow.add(timeBoxSequence.intervalBoxTime, 'minutes');
+		timeBoxHours.push(startNow.format("LT"));
+		finishTime.format("LT");
+
+	}
+	// 9 hour day plan
+	var hourLength = 9;
+
+	for (var i = 0; i < hourLength; i++) {
+		// div element to make rows
+		var planRow = $('<div></div>');
+
+		// div element for hour boxes
+		var hour = $("<div>");
+
+		// user text- textArea element
+		var userMessage = $('<textArea></textArea>');
+
+		// button elements and icons
+		var saveBtn = $('<button></button>');
+		var iconSave = $('<i>');
+		var deleteBtn = $('<button>');
+		var iconDelete = $('<i>')
+
+		// classes, attributes, text added to elements
+		//
+		planRow.addClass("row  time-block");
+		planRow.attr("id", i + 9);
+		//variable to capture planColumn "id"
+		var getID = planRow.attr("id");
+		// hour columns
+		hour.addClass("hour col-1");
+		hour.text(timeBoxHours[i]);
+		// user text
+		userMessage.addClass("col-9")
+		userMessage.attr('value', i + 9);
+		//buttons and icons
+		saveBtn.addClass('saveBtn col-1');
+		saveBtn.attr("value", i + 9);
+		iconSave.addClass("far fa-save");
+		saveBtn.text('SAVE ')
+		deleteBtn.addClass("deleteBtn col-1");
+		deleteBtn.text('DELETE ')
+		deleteBtn.attr("value", i + 9)
+		iconDelete.addClass("fas fa-trash-alt")
+
+		//append element
+		planRow.append(hour);
+		planRow.append(userMessage);
+		saveBtn.append(iconSave);
+		planRow.append(saveBtn);
+		deleteBtn.append(iconDelete)
+		planRow.append(deleteBtn);
+		//append to 'root'  element
+		$("#timeBox-Id").append(planRow);
+
+		// time alert past present future
+
+
+		var textTime = (parseInt(getID))
+		if (textTime < currentTime) {
+			hour.addClass('past');
+		} else if (textTime == currentTime) {
+			hour.addClass('present');
+
+		}
+		else {
+			hour.addClass('future');
+
+		}
+
+
+
+
+
+	}
+	// save button
+	$(".saveBtn").on('click', function (event) {
+		event.preventDefault();
+		// set item time and text
+		var textTime = $(this).siblings("textarea").attr("value");
+		var textInput = $(this).siblings("textarea").val();
+		localStorage.setItem(textTime, textInput)
+
+
+
+
+
+	})
+	// delete button
+	$(".deleteBtn").on('click', function (event) {
+		event.preventDefault();
+		//remove item time and text 
+		var textTime = $(this).siblings("textarea").attr("value");
+		var textInput = $(this).siblings("textarea").val("");
+		localStorage.removeItem(textTime, textInput)
+
+
+
+	})
+
+
+
+
+
+	// 
+	$("textarea").each(function () {
+		var textInput = $(this).attr("value");
+
+		var getTextInput = localStorage.getItem(textInput)
+		$(this).parent().children("textarea").val(getTextInput)
+
+
+	})
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
